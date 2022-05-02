3e91ae45cd92ae6839912d02b74f0d4eb4ab35fa
Rahul G <rahulganga023@gmail.com>
2022-05-02 14:06:54 +0530

    changes made in router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import SearchPage from "./Components/SearchPage/SearchPage";
 import CountryInfoPage from "./Components/CountryInfoPage/CountryInfoPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   let [country, setCountry] = useState<string>("");
@@ -13,18 +13,18 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <HashRouter basename="/">
         <Routes>
           <Route
-            path="/search-countries/"
+            path="/"
             element={<SearchPage handleSubmit={handleSubmit} />}
           />
           <Route
-            path="/search-countries/countryinfo/:country"
+            path="/countryinfo/:country"
             element={<CountryInfoPage countryName={country} />}
           />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
